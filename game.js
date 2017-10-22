6d6e9ae23f0bbb591aec7827fe0104e04d1e0729
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-10-22 15:35:12 +0200

    Added functionality of reset button to reset the score of current game

--- game.js
+++ game.js
@@ -4,76 +4,73 @@ const smallShark = "fa fa-fighter-jet fa-3x"
 const largeShark = "fa fa-fighter-jet fa-4x"
 const currentScore = getElement("score", 0);
 const startButton = document.getElementById("start-btn")
-const resetButton = document.getElementById("reset-btn")
 
 startButton.addEventListener('click', gameFunction)
-
-resetButton.addEventListener('click', resetGameFunction)
 
 function resetGameFunction() {
   currentScore.innerHTML = 0;
 }
 
-  function gameFunction() {
-    setInterval(function getRandomElement() {
-      var selected = getElement("invisible", getRandomNumber(9));
-      selected.className = "visible";
-      var ship = selected.getElementsByClassName("fa fa-ship")[0];
+function gameFunction() {
+  setInterval(function getRandomElement() {
+    var selected = getElement("invisible", getRandomNumber(9));
+    selected.className = "visible";
+    var ship = selected.getElementsByClassName("fa fa-ship")[0];
 
-      if (ship) {
-        setTimeout(function increaseSize() {
-          ship.className = largeShip;
-        },800);
-      }
+    if (ship) {
+      setTimeout(function increaseSize() {
+        ship.className = largeShip;
+      },800);
+    }
 
-      var getPointFunction = function() { getPoint(selected) }
+    var getPointFunction = function() { getPoint(selected) }
 
-      selected.addEventListener( 'click', getPointFunction);
-
-      setTimeout(function changeToInvisible() {
-        selected.className = "invisible";
-        changeToSharkOrShip(selected.firstChild);
-        selected.removeEventListener('click', getPointFunction);
-      },2000);
-    },3000);
-    startButton.id = "reset-btn";
-    startButton.innerHTML = "Reset Game!";
-    startButton.removeEventListener('click', gameFunction);
-  }
-
-  function getRandomNumber(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-  function getElement(className, num) {
-    return document.getElementsByClassName(className)[num];
-  }
-
-  function getPoint(element) {
-    element.classList.add("red");
+    selected.addEventListener( 'click', getPointFunction);
 
     setTimeout(function changeToInvisible() {
-      element.className = "invisible";
-    },200);
+      selected.className = "invisible";
+      changeToSharkOrShip(selected.firstChild);
+      selected.removeEventListener('click', getPointFunction);
+    },2000);
+  },3000);
+  startButton.id = "reset-btn";
+  startButton.innerHTML = "Reset Game!";
+  startButton.removeEventListener('click', gameFunction);
+  document.getElementById("reset-btn").addEventListener('click', resetGameFunction)
+}
 
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
 
-    var elementsFirstChild = element.firstChild
+function getElement(className, num) {
+  return document.getElementsByClassName(className)[num];
+}
 
-    if (((elementsFirstChild.className == smallShark) || (elementsFirstChild.className == largeShark)) && currentScore.innerHTML > 0) {
-      currentScore.innerHTML--;
-    } else if (elementsFirstChild.className == smallShip) {
-      currentScore.innerHTML = parseInt(currentScore.innerHTML) + 2;
-    } else if (elementsFirstChild.className == largeShip) {
-      currentScore.innerHTML = parseInt(currentScore.innerHTML) + 1;
-    }
+function getPoint(element) {
+  element.classList.add("red");
+
+  setTimeout(function changeToInvisible() {
+    element.className = "invisible";
+  },200);
+
+  var elementsFirstChild = element.firstChild
+
+  if (((elementsFirstChild.className == smallShark) || (elementsFirstChild.className == largeShark)) && currentScore.innerHTML > 0) {
+    currentScore.innerHTML--;
+  } else if (elementsFirstChild.className == smallShip) {
+    currentScore.innerHTML = parseInt(currentScore.innerHTML) + 2;
+  } else if (elementsFirstChild.className == largeShip) {
+    currentScore.innerHTML = parseInt(currentScore.innerHTML) + 1;
   }
+}
 
-  function changeToSharkOrShip(elementsFirstChild) {
-    var num = getRandomNumber(10)
-    if (num <= 3) {
-      elementsFirstChild.className = largeShark;
-    } else {
-      elementsFirstChild.className = smallShip;
-    }
+function changeToSharkOrShip(elementsFirstChild) {
+  var num = getRandomNumber(10)
+  if (num <= 3) {
+    elementsFirstChild.className = largeShark;
+  } else {
+    elementsFirstChild.className = smallShip;
   }
+}
 
