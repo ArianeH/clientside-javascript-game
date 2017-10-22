234936fb5733d9ae7cbefc383694cb17387e6ba6
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-10-22 14:37:00 +0200

    Adjusted score tracker to add different amount of points depending on size of element clicked on

--- game.js
+++ game.js
@@ -4,13 +4,15 @@ const smallShark = "fa fa-fighter-jet fa-3x"
 const largeShark = "fa fa-fighter-jet fa-4x"
 
 setInterval(function getRandomElement() {
-  var selected = getElement("invisible", getRandomNumber(1));
+  var selected = getElement("invisible", getRandomNumber(9));
   selected.className = "visible";
   var ship = selected.getElementsByClassName("fa fa-ship")[0];
 
-  setTimeout(function increaseSize() {
-    ship.className = largeShip;
-  },800);
+  if (ship) {
+    setTimeout(function increaseSize() {
+      ship.className = largeShip;
+    },800);
+  }
 
   var getPointFunction = function() { getPoint(selected) }
 
@@ -32,27 +34,28 @@ function getElement(className, num) {
 }
 
 function getPoint(element) {
-  // element.classList.add("red").animationDelay = "2s";
-  element.className = "invisible";
+  element.classList.add("red");
+
+  setTimeout(function changeToInvisible() {
+    element.className = "invisible";
+  },200);
 
   var currentScore = getElement("score", 0);
   var elementsFirstChild = element.firstChild
 
   if (((elementsFirstChild.className == smallShark) || (elementsFirstChild.className == largeShark)) && currentScore.innerHTML > 0) {
     currentScore.innerHTML--;
-  } else if ((elementsFirstChild.className == smallShip) || (elementsFirstChild.className == largeShip)) {
-    currentScore.innerHTML++;
+  } else if (elementsFirstChild.className == smallShip) {
+    currentScore.innerHTML = parseInt(currentScore.innerHTML) + 2;
+  } else if (elementsFirstChild.className == largeShip) {
+    currentScore.innerHTML = parseInt(currentScore.innerHTML) + 1;
   }
-
-  // changeToSharkOrShip(elementsFirstChild);
 }
 
 function changeToSharkOrShip(elementsFirstChild) {
   var num = getRandomNumber(10)
   if (num <= 3) {
-    console.log("HEEEEELLLLLOOOOO!!!!!!!!!!!!!!")
     elementsFirstChild.className = largeShark;
-        console.log("HEEEEELLLLLOOOOO!!!!!!!!!!!!!!")
   } else {
     elementsFirstChild.className = smallShip;
   }
