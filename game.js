3ad36afa6619dda4342b89cb92443ec8003854f6
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-10-28 22:39:07 +0200

    Adjusted visability of items after scrolling to next level

--- game.js
+++ game.js
@@ -4,6 +4,7 @@ const smallShark = "fa fa-fighter-jet fa-3x";
 const largeShark = "fa fa-fighter-jet fa-4x";
 const currentScore = getElement("score", 0);
 const startButton1 = document.getElementById("start-btn-1");
+const gameTable = getElement("game-table", 0);
 
 startButton1.addEventListener('click', gameFunctionLevel1)
 
@@ -31,9 +32,9 @@ function gameFunctionLevel1() {
 }
 
 function gameFunctionLevel2() {
-  var gameTable = getElement("game-table", 0);
   gameTable.id = "game-2";
 
+  console.log("IN GAME 2")
   runTimer("timer-1");
 
   setInterval(function() { getRandomElement() },3000);
@@ -45,8 +46,12 @@ function gameFunctionLevel2() {
 
 function getRandomElement() {
   if (startTime == 0) {
-    scrollNextLevel();
-    return;
+    if (gameTable.id == "game-1") {
+      scrollNextLevel();
+      return;
+    } else {
+      return;
+    }
   }
   var selected = getElement("invisible", getRandomNumber(9));
   selected.className = "visible";
@@ -99,13 +104,14 @@ function getPoint(element) {
 
   var elementsFirstChild = element.firstChild
 
-  if (((elementsFirstChild.className == smallShark) || (elementsFirstChild.className == largeShark)) && currentScore.innerHTML > 0) {
+  if (((elementsFirstChild.className == smallShark) || (elementsFirstChild.className == largeShark))
+    && currentScore.innerHTML > 0) {
     currentScore.innerHTML--;
-  } else if (elementsFirstChild.className == smallShip) {
-    currentScore.innerHTML = parseInt(currentScore.innerHTML) + 2;
-  } else if (elementsFirstChild.className == largeShip) {
-    currentScore.innerHTML = parseInt(currentScore.innerHTML) + 1;
-  }
+} else if (elementsFirstChild.className == smallShip) {
+  currentScore.innerHTML = parseInt(currentScore.innerHTML) + 2;
+} else if (elementsFirstChild.className == largeShip) {
+  currentScore.innerHTML = parseInt(currentScore.innerHTML) + 1;
+}
 }
 
 function changeToSharkOrShip(elementsFirstChild) {
